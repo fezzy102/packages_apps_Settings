59cd09af73d10e094ffa89cb4bb8e3fc0e4db9f3
Alex <du.alexcruz@gmail.com>
2019-02-11 17:18:50 -0600

    Active edge settings [6/6]
    Change-Id: Ie096239065fee6206e134fa812b788b53baff167

--- src/com/android/settings/search/SearchIndexableResourcesImpl.java
+++ src/com/android/settings/search/SearchIndexableResourcesImpl.java
@@ -97,7 +97,9 @@ import com.android.settings.wallpaper.WallpaperTypeSettings;
 import com.android.settings.wfd.WifiDisplaySettings;
 import com.android.settings.wifi.ConfigureWifiSettings;
 import com.android.settings.wifi.WifiSettings;
+import com.aquarios.coralreef.fragments.ActiveEdge;
 import com.aquarios.coralreef.fragments.GestureOptions;
+
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -193,6 +195,9 @@ public class SearchIndexableResourcesImpl implements SearchIndexableResources {
         addIndex(RecentLocationRequestSeeAllFragment.class);
         addIndex(PreviouslyConnectedDeviceDashboardFragment.class);
         addIndex(BluetoothDashboardFragment.class);
+
+        // CoralReef
+        addIndex(ActiveEdge.class);
     }
 
     @Override
