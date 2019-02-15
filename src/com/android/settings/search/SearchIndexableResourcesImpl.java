5b2e5dbb4dc79de1e9369571b08e824364d53fbb
calebcabob <calphonic@gmail.com>
2019-02-14 23:46:51 -0500

    Revert "Move gestures to CoralReef [1/2]"
    This reverts commit 816988812a0d3460c6a4fed76e4ea6acecbce3ab.

--- src/com/android/settings/search/SearchIndexableResourcesImpl.java
+++ src/com/android/settings/search/SearchIndexableResourcesImpl.java
@@ -60,6 +60,7 @@ import com.android.settings.gestures.DoubleTapPowerSettings;
 import com.android.settings.gestures.DoubleTapScreenSettings;
 import com.android.settings.gestures.DoubleTwistGestureSettings;
 import com.android.settings.gestures.SwipeUpGestureSettings;
+import com.android.settings.gestures.GestureSettings;
 import com.android.settings.gestures.PickupGestureSettings;
 import com.android.settings.gestures.SwipeToNotificationSettings;
 import com.android.settings.inputmethod.AvailableVirtualKeyboardFragment;
@@ -97,7 +98,6 @@ import com.android.settings.wallpaper.WallpaperTypeSettings;
 import com.android.settings.wfd.WifiDisplaySettings;
 import com.android.settings.wifi.ConfigureWifiSettings;
 import com.android.settings.wifi.WifiSettings;
-import com.aquarios.coralreef.fragments.GestureOptions;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -140,7 +140,7 @@ public class SearchIndexableResourcesImpl implements SearchIndexableResources {
         addIndex(DoubleTwistGestureSettings.class);
         addIndex(SwipeUpGestureSettings.class);
         addIndex(SwipeToNotificationSettings.class);
-        addIndex(GestureOptions.class);
+        addIndex(GestureSettings.class);
         addIndex(LanguageAndInputSettings.class);
         addIndex(LocationSettings.class);
         addIndex(ScanningSettings.class);
